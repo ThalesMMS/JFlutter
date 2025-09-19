f36fe32fb08bc1adf5d69afefe5b3e3d5896866b
Thales Matheus <133025183+ThalesMMS@users.noreply.github.com>
2025-09-18 22:43:16 -0300

    Add Flutter app delegate launch unit test

--- ios/RunnerTests/RunnerTests.swift
+++ ios/RunnerTests/RunnerTests.swift
@@ -1,12 +1,27 @@
 import Flutter
 import UIKit
 import XCTest
+@testable import Runner
 
 class RunnerTests: XCTestCase {
+  func testFlutterAppDelegateSetsUpWindowAndRootController() {
+    let application = UIApplication.shared
+    let appDelegate = AppDelegate()
 
-  func testExample() {
-    // If you add code to the Runner application, consider adding tests here.
-    // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+    let didFinishLaunching = appDelegate.application(application, didFinishLaunchingWithOptions: nil)
+
+    XCTAssertTrue(didFinishLaunching, "AppDelegate should report a successful launch")
+    XCTAssertNotNil(appDelegate.window, "AppDelegate should create the main window")
+
+    let rootViewController = appDelegate.window?.rootViewController
+    XCTAssertNotNil(rootViewController, "AppDelegate should assign a rootViewController")
+    XCTAssertTrue(
+      rootViewController is FlutterViewController,
+      "Root view controller should be a FlutterViewController"
+    )
+
+    let flutterViewController = rootViewController as? FlutterViewController
+    flutterViewController?.loadViewIfNeeded()
+    XCTAssertNotNil(flutterViewController?.view, "FlutterViewController should load its view successfully")
   }
-
 }

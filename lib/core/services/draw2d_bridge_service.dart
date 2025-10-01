dabaf7d9c340b3d132493bf4065e8771cae6485d
Thales Matheus <133025183+ThalesMMS@users.noreply.github.com>
2025-10-01 15:52:28 -0300

    simulation: bridge draw2d highlight events

---
+++ lib/core/services/draw2d_bridge_service.dart
@@ -0,0 +1,32 @@
+import 'draw2d_bridge_platform_stub.dart'
+    if (dart.library.html) 'draw2d_bridge_platform_web.dart';
+
+/// Singleton facade responsible for communicating with the Draw2D runtime.
+class Draw2DBridgeService {
+  Draw2DBridgeService._(this._platform);
+
+  factory Draw2DBridgeService() => _instance;
+
+  static final Draw2DBridgeService _instance =
+      Draw2DBridgeService._(createDraw2DBridgePlatform());
+
+  final Draw2DBridgePlatform _platform;
+
+  /// Dispatches a highlight event to the Draw2D runtime.
+  void highlight({
+    required Set<String> states,
+    required Set<String> transitions,
+  }) {
+    final payload = <String, dynamic>{
+      'states': states.toList(),
+      'transitions': transitions.toList(),
+    };
+
+    _platform.postMessage('highlight', payload);
+  }
+
+  /// Dispatches a request to clear all highlights.
+  void clearHighlight() {
+    _platform.postMessage('clear_highlight', const {});
+  }
+}
